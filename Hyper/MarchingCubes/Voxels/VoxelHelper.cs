9704d71cef912b3bacfd7685de581e7785cf7788
Karol Denst <kdenst@gmail.com>
2023-07-24 22:21:07 +0200

    updated colors

--- Hyper/MarchingCubes/Voxels/VoxelHelper.cs
+++ Hyper/MarchingCubes/Voxels/VoxelHelper.cs
@@ -8,9 +8,9 @@ public static class VoxelHelper
     {
         return type switch
         {
-            (VoxelType.Grass) => new Vector3(0, 1, 0),
+            (VoxelType.Grass) => new Vector3(0.1f, 0.7f, 0.1f),
             (VoxelType.GrassRock) => new Vector3(0.5f, 0.5f, 0),
-            (VoxelType.Rock) => new Vector3(1, 0, 0),
+            (VoxelType.Rock) => new Vector3(0.7f, 0.1f, 0.1f),
             _ => Vector3.Zero
         };
     }
