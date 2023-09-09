a26db5f3b62f60cdf5b9168cce8fd3a2ab7cf9d0
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2023-09-09 14:12:31 +0200

    fix bounding boxes rendering

--- Hyper/Controllers/BotsController.cs
+++ Hyper/Controllers/BotsController.cs
@@ -1,5 +1,4 @@
 ﻿using Character.Shaders;
-using Common;
 using Common.UserInput;
 using Hyper.Shaders;
 using OpenTK.Mathematics;
@@ -12,9 +11,9 @@ internal class BotsController : IController, IInputSubscriber
 
     private readonly ModelShader _shader;
 
-    private readonly Shader _objectShader;
+    private readonly ObjectShader _objectShader;
 
-    public BotsController(Scene scene, ModelShader shader, Shader objectShader)
+    public BotsController(Scene scene, ModelShader shader, ObjectShader objectShader)
     {
         _scene = scene;
         _shader = shader;
@@ -25,15 +24,17 @@ internal class BotsController : IController, IInputSubscriber
     public void Render()
     {
         _shader.SetUp(_scene.Camera, _scene.LightSources, _scene.Scale);
-
         foreach (var bot in _scene.Bots)
         {
             bot.Render(_shader, _scene.Scale, _scene.Camera.ReferencePointPosition);
-
-#if BOUNDING_BOXES
-            bot.PhysicalCharacter.RenderBoundingBox(_objectShader, _scene.Scale, _scene.Camera.ReferencePointPosition);
-#endif
         }
+#if BOUNDING_BOXES
+        _objectShader.SetUp(_scene.Camera, _scene.LightSources, _scene.Scale);
+        foreach (var bot in _scene.Bots)
+        {
+            bot.PhysicalCharacter.RenderBoundingBox(_objectShader, _scene.Scale, _scene.Camera.ReferencePointPosition);
+        }
+#endif
     }
 
     public void RegisterCallbacks()
--- Hyper/Controllers/PlayerController.cs
+++ Hyper/Controllers/PlayerController.cs
@@ -1,5 +1,4 @@
 ﻿using Character.Shaders;
-using Common;
 using Common.UserInput;
 using Hyper.Shaders;
 using OpenTK.Mathematics;
@@ -13,9 +12,9 @@ internal class PlayerController : IController, IInputSubscriber
 
     private readonly ModelShader _shader;
 
-    private readonly Shader _objectShader;
+    private readonly ObjectShader _objectShader;
 
-    public PlayerController(Scene scene, ModelShader shader, Shader objectShader)
+    public PlayerController(Scene scene, ModelShader shader, ObjectShader objectShader)
     {
         _scene = scene;
         _shader = shader;
@@ -26,11 +25,11 @@ internal class PlayerController : IController, IInputSubscriber
     public void Render()
     {
         _shader.SetUp(_scene.Camera, _scene.LightSources, _scene.Scale);
-
+        _scene.Player.Render(_shader, _scene.Scale, _scene.Camera.ReferencePointPosition, _scene.Camera.FirstPerson);
 #if BOUNDING_BOXES
+        _objectShader.SetUp(_scene.Camera, _scene.LightSources, _scene.Scale);
         _scene.Player.PhysicalCharacter.RenderBoundingBox(_objectShader, _scene.Scale, _scene.Camera.ReferencePointPosition);
 #endif
-        _scene.Player.Render(_shader, _scene.Scale, _scene.Camera.ReferencePointPosition, _scene.Camera.FirstPerson);
     }
 
     public void RegisterCallbacks()
