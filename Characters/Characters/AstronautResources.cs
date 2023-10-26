b3d477a8e030e3ed0c2bb700ed399654767e626b
Karol Denst <kdenst@gmail.com>
2023-10-26 21:11:18 +0200

    dunno

---
+++ Characters/Characters/AstronautResources.cs
@@ -0,0 +1,13 @@
+﻿namespace Character.Characters;
+
+public class AstronautResources : ModelResources
+{
+    private static readonly Lazy<AstronautResources> InternalInstance = new(() => new AstronautResources());
+
+    public static AstronautResources Instance => InternalInstance.Value;
+
+    public AstronautResources() : base(
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources/Astronaut/model.dae"),
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources/Astronaut/texture.png"))
+    { }
+}
